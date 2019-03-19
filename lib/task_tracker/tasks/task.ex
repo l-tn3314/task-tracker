ab5ea10cf9700f735a84fdf129b8bc86026c4a0f
l-tn3314 <l.tn3314@gmail.com>
2019-03-19 12:27:04 -0400

    add resources, pw deps

---
+++ lib/task_tracker/tasks/task.ex
@@ -0,0 +1,22 @@
+defmodule TaskTracker.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "tasks" do
+    field :completed, :boolean, default: false
+    field :description, :string
+    field :time_spent, :integer, default: 0
+    field :title, :string
+    belongs_to :user, TaskTracker.Users.User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:title, :description, :completed, :time_spent])
+    |> validate_required([:title, :description, :completed, :time_spent])
+  end
+end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TaskTracker.Repo
+alias TaskTracker.Users.User
+alias TaskTracker.Tasks.Task
+
+pwhash = Argon2.hash_pwd_salt("pass123")
+
+Repo.insert!(%User{email: "test@test.com", password_hash: pwhash})
+Repo.insert!(%User{email: "abc@test.com", password_hash: pwhash})
+Repo.insert!(%User{email: "qwe@test.com", password_hash: pwhash})
+
+Repo.insert!(%Task{title: "some task", description: "some task description"})
+Repo.insert!(%Task{title: "another task", description: "another task description"})
