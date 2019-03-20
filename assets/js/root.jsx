526d9ca270d1e36f6b384f040aab00555a42fbb8
l-tn3314 <l.tn3314@gmail.com>
2019-03-19 21:25:39 -0400

    add router

--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function root_init(node) {
   let tasks = window.tasks;
@@ -8,20 +9,57 @@ export default function root_init(node) {
 }
 
 class Root extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+    };
+  } 
+ 
+  fetch_users() {
+    $.ajax("/api/users", {
+      method: "get",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: "",
+      success: (resp) => {
+        console.log("yes");
+        this.setState(_.assign({}, this.state, { users: resp.data }));
+      },
+      error: (resp) => {
+        console.log("oh no");
+      },
+    });
+  }
+
   render() {
     return <div className="container">
-        <Header />
-        <TaskList tasks={this.props.tasks} />
+        <Router>
+          <div>
+            <Header root={this} />
+            <Route path="/" exact={true} render={() =>
+              <TaskList tasks={this.props.tasks} />
+            } />
+            <Route path="/users" exact={true} render={() =>
+              <UserList users={this.state.users} />
+            } />
+          </div>
+        </Router>
       </div>;
   }
 }
 
-function Header(_props) {
+function Header(props) {
+  let {root} = props;
+
   return <div>
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <div className="col-7">
-        <a className="navbar-brand" href="/">Task Tracker</a>
+      <div className="col-5">
+        <h2><Link to={"/"}>Task Tracker</Link></h2>
+      </div>
+      <div className="col-2">
+        <p><Link to={"/users"} onClick={root.fetch_users.bind(root)}>Users</Link></p>
       </div>
       <div className="col-5">
         <div className="form-inline my-2">
@@ -49,5 +87,32 @@ function Task(props) {
         <p className="card-text">{task.description}</p>
       </div>
     </div>;
+}
+
+function UserList(props) {
+  let users = _.map(props.users, (user) => <User key={user.id} user={user} />);
+
+  return <div className="row">
+      <div className="col-12">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users}
+          </tbody>
+        </table>
+      </div>
+    </div>;
+}
+
+function User(props) {
+  let {user} = props;
+
+  return <tr>
+      <td>{user.email}</td>
+    </tr>;  
 }
 
